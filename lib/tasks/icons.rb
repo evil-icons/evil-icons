a7a93af8d9802889711fe9a4e0952b2ea6749b67
Alexander Madyankin <alexander@madyankin.name>
2014-11-21 13:35:09 +0600

    Add svg filenames normalization

--- lib/tasks/icons.rb
+++ lib/tasks/icons.rb
@@ -8,10 +8,26 @@ doc_path  = File.join(root, 'index.html')
 namespace :evil_icons do
 
   desc "Generate SVG icons sprite"
-  task :process do
+  task :process => :normalize_filenames do
     generator = EvilIcons::Generator.new(svg_path)
     generator.write(res_path, 'icons')
     generator.write(doc_path, 'index')
+  end
+
+
+  desc "Normalize filenames"
+  task :normalize_filenames do
+    filenames = Dir.entries(svg_path).select { |f| File.extname(f) == '.svg' }
+
+    filenames.each do |old_name|
+      next unless old_name.include?('_')
+
+      new_name = File.join svg_path, old_name.gsub('_', '-')
+      old_name = File.join svg_path, old_name
+
+      File.delete(new_name) if File.exists?(new_name)
+      File.rename(old_name, new_name)
+    end
   end
 
 end
