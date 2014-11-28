2871f09e3fd5d82f7255763ab921130082dd6791
Alexander Madyankin <alexander@madyankin.name>
2014-11-28 20:52:17 +0600

    Cleanup

--- lib/tasks/icons.rb
+++ lib/tasks/icons.rb
@@ -3,7 +3,6 @@ require "evil_icons/generator"
 root      = File.expand_path('../../../', __FILE__)
 svg_path  = File.join(root, 'app', 'assets', 'images',     'evil-icons')
 res_path  = File.join(root, 'app', 'views',  'evil_icons', '_icons.erb')
-doc_path  = File.join(root, 'index.html')
 
 namespace :evil_icons do
 
@@ -11,7 +10,6 @@ namespace :evil_icons do
   task :process => :normalize_filenames do
     generator = EvilIcons::Generator.new(svg_path)
     generator.write(res_path, 'icons')
-    generator.write(doc_path, 'index')
   end
 
 
