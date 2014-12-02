95d12a8c330e0e0810b562a6aa83ae76f0245a60
Andrey Deryabin <deriabin@gmail.com>
2014-12-03 02:44:33 +0300

    remove rails dependency add sinatra support

--- app/helpers/evil_icons/helpers.rb
+++ app/helpers/evil_icons/helpers.rb
@@ -2,20 +2,26 @@ module EvilIcons
   module Helpers
 
     def evil_icons_sprite
-      render "evil_icons/icons"
+      template = ERB.new File.new(File.join(File.dirname(File.dirname(File.dirname(__FILE__))), "views", "evil_icons", "_icons.erb")).read
+      rails_condition template.result
     end
 
     def evil_icon(name, options = {})
       size  = options[:size] ? "icon--#{options[:size]}" : ''
       options[:class] = "icon icon--#{name} #{size} #{options[:class]}"
 
-      content_tag :div, options do
-        content_tag :svg, class: "icon__cnt" do
-          tag :use, "xlink:href" => "##{name}-icon"
-        end
-      end
+      rails_condition "<div class='#{options[:class]}'><svg class='icon__cnt'><use xlink:href='##{name}-icon'/></svg></div>"
 
     end
 
+    private
+
+    def rails_condition(html)
+      if defined? Rails
+        html.html_safe
+      else
+        html
+      end
+    end
   end
 end
