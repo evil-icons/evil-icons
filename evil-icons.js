24de676e7978b4f49042bbebd26d21d7b0d3e286
Alexander Madyankin <alexander@madyankin.name>
2015-01-17 18:26:40 +0600

    Add iconizeHtml method to the NPM package

--- evil-icons.js
+++ evil-icons.js
@@ -1,7 +1,27 @@
-var fs = require('fs');
-var spritePath = __dirname + "/assets/sprite.svg";
+var fs          = require('fs');
+var spritePath  = __dirname + "/assets/sprite.svg";
+var sprite      = fs.readFileSync(spritePath).toString();
 
-var wrapSpinner = function (html, klass) {
+
+function icon(name, options) {
+  var options = options || {};
+  var size    = options.size ? "icon--" + options.size : "";
+  var klass   = "icon icon--" + name + " " + size + " " + (options.class || "");
+
+
+  var icon =  "<svg class='icon__cnt'>" +
+                "<use xlink:href='#" + name + "-icon' />" +
+              "</svg>";
+
+  var html =  "<div class='" + klass + "'>" +
+                wrapSpinner(icon, klass) +
+              "</div>";
+
+  return html;
+}
+
+
+function wrapSpinner(html, klass) {
   if (klass.indexOf("spinner") > -1) {
     return "<div class='icon__spinner'>" + html + "</div>";
   } else {
@@ -10,25 +30,51 @@ var wrapSpinner = function (html, klass) {
 }
 
 
-module.exports = {
+function buildParamsFromString(string) {
+  var paramsString;
+  var params = {};
+  var string = string.trim().replace(/['"]/gi, '');
 
-  sprite: fs.readFileSync(spritePath).toString(),
+  string.split(' ').forEach(function(param){
+    var param = param.split('=');
+    var key   = param[0];
+    var value = param[1];
 
-  icon: function icon(name, options) {
-    var options = options || {};
-    var size    = options.size ? "icon--" + options.size : "";
-    var klass   = "icon icon--" + name + " " + size + " " + (options.class || "");
+    params[key] = value;
+  });
+
+  return params;
+}
 
 
-    var icon =  "<svg class='icon__cnt'>" +
-                  "<use xlink:href='#" + name + "-icon' />" +
-                "</svg>";
+function replaceIconTags(src) {
+  var match, tag, params, name;
+  var html = src.toString();
+  var iconRegexp  = /<icon\s+([-=\w\d'"\s]+)\s*\/?>(<\/icon>)?/gi;
 
-    var html =  "<div class='" + klass + "'>" +
-                  wrapSpinner(icon, klass) +
-                "</div>";
+  while (match = iconRegexp.exec(html)) {
+    tag     = match[0];
+    params  = buildParamsFromString(match[1]);
+    name    = params.name;
 
-    return html;
+    delete params.name;
+
+    html = html.replace(tag, icon(name, params));
   }
 
+  return html;
+}
+
+
+function iconizeHtml(src) {
+  var html  = src.toString();
+  html = html.replace(/<body.*?>/, function(match) { return match + sprite });
+  return replaceIconTags(html);
+}
+
+
+module.exports = {
+  iconizeHtml:  iconizeHtml,
+  sprite:       sprite,
+  icon:         icon
 };
--- lib/evil_icons/version.rb
+++ lib/evil_icons/version.rb
@@ -1,3 +1,3 @@
 module EvilIcons
-  VERSION = "1.4.0"
+  VERSION = "1.5.0"
 end
