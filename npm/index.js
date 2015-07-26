d38458c4d5e375f96d1606c4740a9f1259230f2b
Alexander Madyankin <alexander@madyankin.name>
2015-07-26 21:49:08 +0300

    Rename js -> lib

--- npm/index.js
+++ npm/index.js
@@ -1,6 +1,6 @@
-import icon        from './js/icon';
-import sprite      from './js/sprite';
-import iconizer    from './js/iconizer';
-import { setDirs } from './js/files';
+import icon        from './lib/icon';
+import sprite      from './lib/sprite';
+import iconizer    from './lib/iconizer';
+import { setDirs } from './lib/files';
 
 export default { sprite, icon, setDirs, iconizeHtml: iconizer };
